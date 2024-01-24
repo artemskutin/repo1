711bf8a2b023dfd80cc2886525356fbd02a7d384
Skutin Artem <Skutinartem5@gmail.com>
2024-01-24 21:11:53 +0300

    add lesson1:task1

--- Desktop/Lesson222/task1/Program.cs
+++ Desktop/Lesson222/task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello World");
+﻿//Console.WriteLine("Hello World");
+
+
+int velem =100;
+Console.WriteLine(velem);
+
+double d = 5.5;
+char c = '!';
+bool b = true; 
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
