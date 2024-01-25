e1ce2515a276ced6554b874a1f59840890238df7
Skutin Artem <Skutinartem5@gmail.com>
2024-01-25 22:25:35 +0300

    add lesson:task5

---
+++ Desktop/Lesson222/task5/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+
+int n = 25;
+int i = 1;
+
+while (i <= n)
+{
+    //Console.WriteLine(i);               // вывод в столбик 
+    Console.Write(i);              //  вывод в строку
+    Console.Write(' ');               // вывод с пробелом для хорошей читаемости в строку 
+    i = i + 1;
+}
